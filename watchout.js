42b84823f3a93d67cfa3101d7498c0cd012ef370
skeller88 <skeller88@gmail.com>
2014-03-07 21:44:44 -0800

    draw enemies in svg element

--- watchout.js
+++ watchout.js
@@ -1,48 +1,61 @@
 var gameOptions = {
   width: 450,
   height: 700,
-  nEnemies: 10
-}
-var enemyData = [{ "class": "enemy", "height": 25, "width": 50, "xlink:href": "asteroid.png" }];
+  nEnemies: 10,
+  enemySize: 10 //radius of one enemy
+};
 
-var gameBoard = d3.select('.container').append('svg:svg')
-                .attr('width', gameOptions.width)
-                .attr('height', gameOptions.height)
+var axes = {
+  x: d3.scale.linear().domain([0, 100]).range([0, gameOptions.width - gameOptions.enemySize * 2]), //TODO - fix so that no part of an enemy can be off the gameboard
+  y: d3.scale.linear().domain([0, 100]).range([0, gameOptions.height - gameOptions.enemySize * 2])
+};
+
+var gameBoard = d3.select('.container').append('svg:svg').attr('width', gameOptions.width).attr('height', gameOptions.height);
 
 // var svgContainer = d3.select("svg"); //todo - trying to append svg to body results in "cannot call attr of null" error
 
-var enemyGroup = gameBoard.append("g");
-var enemies = enemyGroup.selectAll("enemy").data(enemyData).enter().append('enemyGroup');
+//ENTER
 
+// var circleAttributes = circles
+// // 24                       .attr("cx", function (d) { return d.cx; })
+// // 25                       .attr("cy", function (d) { return d.cy; })
+// // 26                       .attr("r", function (d) { return d.radius; })
+// // 27                       .style("fill", function (d) { return d.color; });
 
-// 10var svgContainer = d3.select("body").append("svg")
-// 11                                     .attr("width",200)
-// 12                                     .attr("height",200);
-// 13
-// 14//Add a group to hold the circles
-// 15var circleGroup = svgContainer.append("g");
-// 16
-// 17//Add circles to the circleGroup
-// 18var circles = circleGroup.selectAll("circle")
-// 19                          .data(circleData)
-// 20                          .enter()
-// 21                          .append("circle");
-// 22
-// 23var circleAttributes = circles
-// 24                       .attr("cx", function (d) { return d.cx; })
-// 25                       .attr("cy", function (d) { return d.cy; })
-// 26                       .attr("r", function (d) { return d.radius; })
-// 27                       .style("fill", function (d) { return d.color; });
-// 28
-// 29// * Note * that the rectangles are added to the svgContainer, not the circleGroup
-// 30var rectangles = svgContainer.selectAll("rect")
-// 31                              .data(rectangleData)
-// 32                              .enter()
-// 33                              .append("rect");
-// 34
-// 35var rectangleAttributes = rectangles
-// 36                          .attr("x", function (d) { return d.rx; })
-// 37                          .attr("y", function (d) { return d.ry; })
-// 38                          .attr("height", function (d) { return d.height; })
-// 39                          .attr("width", function (d) { return d.width; })
-// 40                          .style("fill", function(d) { return d.color; });
+var render = function(newEnemyPositions){
+  //UPDATE- match up all selected elements with newEnemyPositions data, based on each enemy id
+  enemies = gameBoard.selectAll('circle.enemy').data(newEnemyPositions, function(d) {
+    return d.id;
+  });
+
+  //ENTER- when initializing the app
+  enemies.enter().append('svg:circle').attr('class', 'enemy').attr('cx', function(enemy) {
+    return axes.x(enemy.x);
+  }).attr('cy', function(enemy) {
+    return axes.y(enemy.y);
+  }).attr('r', function(enemy) {
+    console.log(gameOptions.enemySize);
+    return gameOptions.enemySize;
+  });
+
+  //REMOVE - not needed? 
+  enemies.exit().remove();
+};
+
+var createEnemies = function() {
+  return _.range(0, gameOptions.nEnemies).map(function(i) {
+    return {
+      id: i,
+      x: Math.random() * 100,
+      y: Math.random() * 100
+    }
+  });
+};
+
+var gameTurn = function() {
+  var newEnemyPositions;
+  newEnemyPositions = createEnemies();
+  return render(newEnemyPositions);
+};
+
+gameTurn();
