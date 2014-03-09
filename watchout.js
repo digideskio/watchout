43d815822db5db3da2e872e0f6654f51938f7dd3
skeller88 <skeller88@gmail.com>
2014-03-08 17:40:51 -0800

    Make it so that the enemies move to a new random location every second.

--- watchout.js
+++ watchout.js
@@ -14,7 +14,7 @@ var gameBoard = d3.select('.container').append('svg:svg').attr('width', gameOpti
 
 // var svgContainer = d3.select("svg"); //todo - trying to append svg to body results in "cannot call attr of null" error
 
-//ENTER
+//ENTER 
 
 // var circleAttributes = circles
 // // 24                       .attr("cx", function (d) { return d.cx; })
@@ -22,9 +22,31 @@ var gameBoard = d3.select('.container').append('svg:svg').attr('width', gameOpti
 // // 26                       .attr("r", function (d) { return d.radius; })
 // // 27                       .style("fill", function (d) { return d.color; });
 
+var tweenWithCollisionDetection = function(endData) {
+    var endPos, enemy, startPos;
+    enemy = d3.select(this);
+    startPos = {
+      x: parseFloat(enemy.attr('cx')),
+      y: parseFloat(enemy.attr('cy'))
+    };
+    endPos = {
+      x: axes.x(endData.x),
+      y: axes.y(endData.y)
+    };
+    return function(t) {
+      var enemyNextPos;
+      // checkCollision(enemy, onCollision);
+      enemyNextPos = {
+        x: startPos.x + (endPos.x - startPos.x) * t,
+        y: startPos.y + (endPos.y - startPos.y) * t
+      };
+      return enemy.attr('cx', enemyNextPos.x).attr('cy', enemyNextPos.y);
+    };
+  };
+
 var render = function(newEnemyPositions){
   //UPDATE- match up all selected elements with newEnemyPositions data, based on each enemy id
-  enemies = gameBoard.selectAll('circle.enemy').data(newEnemyPositions, function(d) {
+  var enemies = gameBoard.selectAll('circle.enemy').data(newEnemyPositions, function(d) {
     return d.id;
   });
 
@@ -34,12 +56,13 @@ var render = function(newEnemyPositions){
   }).attr('cy', function(enemy) {
     return axes.y(enemy.y);
   }).attr('r', function(enemy) {
-    console.log(gameOptions.enemySize);
     return gameOptions.enemySize;
   });
 
   //REMOVE - not needed? 
   enemies.exit().remove();
+
+  return enemies.transition().duration(500).attr('r', 10).transition().duration(2000).tween('custom', tweenWithCollisionDetection); 
 };
 
 var createEnemies = function() {
@@ -52,10 +75,21 @@ var createEnemies = function() {
   });
 };
 
-var gameTurn = function() {
-  var newEnemyPositions;
-  newEnemyPositions = createEnemies();
-  return render(newEnemyPositions);
+var play = function() {
+  var gameTurn, increaseScore;
+  gameTurn = function() {
+    var newEnemyPositions;
+    newEnemyPositions = createEnemies();
+    console.log(newEnemyPositions);
+    return render(newEnemyPositions);
+  };
+  // increaseScore = function() {
+  //   gameStats.score += 1;
+  //   return updateScore();
+  // };
+  gameTurn();
+  setInterval(gameTurn, 2000);
+  // return setInterval(increaseScore, 50);
 };
 
-gameTurn();
+play();
